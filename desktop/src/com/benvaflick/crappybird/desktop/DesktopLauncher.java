3913a155981c2011fc59a451a17a83d8e3571e93
SBT-Vyunik-YuS <benvaflick@gmail.com>
2016-08-25 11:04:00 +0300

    RC1

--- desktop/src/com/benvaflick/crappybird/desktop/DesktopLauncher.java
+++ desktop/src/com/benvaflick/crappybird/desktop/DesktopLauncher.java
@@ -1,15 +1,17 @@
 package com.benvaflick.crappybird.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.benvaflick.crappybird.Main;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.width = Main.WIDTH;
-		config.height = Main.HEIGHT;
-		config.title = Main.TITLE;
-		new LwjglApplication(new Main(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = Main.WIDTH;
+        config.height = Main.HEIGHT;
+        config.title = Main.TITLE;
+        config.addIcon("sprites/ico32.png", Files.FileType.Internal);
+        new LwjglApplication(new Main(), config);
+    }
 }
